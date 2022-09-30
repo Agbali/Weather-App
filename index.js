59e2e60006352f060d5e36259e60b4e37477b779
Blessing Agbali <blessagbali20@gmail.com>
2022-09-30 11:30:13 +0100

    Cleaned up the code and made it object oriented Worked on the unit conversion

--- index.js
+++ index.js
@@ -29,96 +29,105 @@ myDates();
 //create a function that will change the h1 country to the country the user will search for
 //Then the temperature of that country is called
 
-function inputKeyUp(event){
-//     //prevent default prevents the page from loading
-           // 
-//     //enter for the Enter key on your keyboard
-    if (event.key === "Enter") {
-        let h1 = document.querySelector("#lagosHaiD");
-        h1.innerHTML = searchInput.value;
-        event.preventDefault();
-    
-        // The search input value is put into a string and passed into the API's url
-        let location = `${searchInput.value}`;
-        let apiKey1 = "76a578de3ab86388efb5bf00ea5c9bf1";
-        let apiUrl2 = `https://api.openweathermap.org/data/2.5/weather?q=${location}&limit=5&appid=${apiKey1}&units=metric`;
-        
-        axios.get(`${apiUrl2}`).then(loadTemp);
-
-        function loadTemp(response) {
-            console.log(response.data.name);//The API returns an array
-            console.log(response.data);
-            //round up the degrees
-            let degrees = Math.round(response.data.main.temp);
-            let searchedCountry = document.querySelector(".removeColor");
-            searchedCountry.innerHTML = `${degrees}°`;
-          }
-
-    }
-    
-    else{
-        let h1 = document.querySelector("#lagosHaiD");
-        h1.innerHTML = "Lagos";
-        
-    }
-
- }
-
-//This selects the form itself as a document
-let searchInput = document.querySelector("#searchBox");
-//onkeypress will connects to when a key on your keyboard is pressed
-//Add event listener to help you choose what you want your program to do
-searchInput.addEventListener("onkeypress", inputKeyUp); 
-
-
-// const fahrenheit = celsius => (celsius * 1.8) + 32
-
-// fahrenheit(59); 
-
-// const fahrenheitToCelsius = fahrenheit => (fahrenheit - 32) * 5/9;
-
-// fahrenheitToCelsius()
-
-
-
-
 //ANother function to be called when the user wants to get the temperature for his/her current location
 //Same function but this time the navigator geolocation function is called
 function giveTemp(response){
     //receives response from the weather API 
-    console.log(response.data);
-    console.log(response.data.main.temp);
-    console.log(response.data.name)
-
-    let roundOff = Math.round(response.data.main.temp);
+    console.log(response.data.weather[0].description);
+    console.log(response.data.main);
+    
+    let degrees = Math.round(response.data.main.temp);
+    //let roundOff = Math.round(response.data.main.temp);
     let h2 = document.querySelector("#symbol");
-    h2.innerHTML = `${roundOff}°`;
+    let locationname = document.querySelector("#lagosHaiD");
+    let iconElement = document.querySelector("#w-icon");
+    let winddir = document.querySelector("#windeck");
+    let humid = document.querySelector("#humidity");
+    //let searchedCountry = document.querySelector(".removeColor");
 
-    let lagos = document.querySelector(".lagosName");
-    lagos.innerHTML = `${response.data.name}`;
-
+    celsiusTemp = Math.round(response.data.main.temp);
+    //searchedCountry.innerHTML = `${degrees}°`;
+    humid.innerHTML = Math.round(response.data.main.humidity)
+    winddir.innerHTML = response.data.wind.speed;
+    locationname.innerHTML = `${response.data.name}`;
+    h2.innerHTML = `${degrees}°`;
+    iconElement.setAttribute(
+        "src", 
+        `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+        );
 }
 
-function tempButton(position){
-    console.log(position);
-    console.log(position.coords.latitude);
-    console.log(position.coords.longitude);
-    //This data of longitude and latitude is provided by the geolocation navigator and is
-    //passed to the API as strings.
-    let lati = position.coords.latitude;
-    let longi = position.coords.longitude;
-    let apiKey = "76a578de3ab86388efb5bf00ea5c9bf1";
-
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lati}&lon=${longi}&appid=${apiKey}&units=metric`;
-
-    //axios helps to use the API then calls the giveTemp function above
-    axios.get(`${apiUrl}`).then(giveTemp);
-}
-//navigator function is nested in this function
-function temperateRedir(){
-    navigator.geolocation.getCurrentPosition(tempButton);
+function searchCity(city) {
+    
+    let apiKey1 = "76a578de3ab86388efb5bf00ea5c9bf1";
+    let apiUrl2 = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey1}&units=metric`;
+    
+    axios.get(apiUrl2).then(giveTemp);
 }
 
-//listens to the button html element
-let buttonClick = document.querySelector("button");
-buttonClick.addEventListener("click", temperateRedir);
+function searchLocation(event){
+    event.preventDefault();
+    //enter for the Enter key on your keyboard
+    let countryInput = document.querySelector("#search-box");
+    searchCity(countryInput.value);    
+        // calls the loadTemp function with this value passed into it
+ }
+
+ function changeToFahrenheit(event){
+    event.preventDefault();
+    let fahrenheitTemperature = (celsiusTemp * 9) / 5 + 32;
+    
+    celsiusLink.classList.remove("inCelsius");
+    FahrenheitLink.classList.add("inCelsius");
+    
+    
+    let temperatureElement = document.querySelector("#symbol");
+    temperatureElement.innerHTML = `${Math.round(fahrenheitTemperature)}°`;
+    //alert(fahrenheitTemperature);
+ }
+
+ function changeBackCelcius(event){
+    event.preventDefault();
+
+    celsiusLink.classList.add("inCelsius");
+    FahrenheitLink.classList.remove("inCelsius");
+
+    let celsiusDegree = document.querySelector("#symbol");
+    celsiusDegree.innerHTML = `${celsiusTemp}°`;
+
+ }
+
+ let celsiusTemp = null;
+//This selects the form itself as a document
+let form = document.querySelector("#search-form");
+//Add event listener to help you choose what you want your program to do
+form.addEventListener("submit", searchLocation); 
+
+let celsiusLink = document.querySelector("#Celsius-link");
+celsiusLink.addEventListener("click", changeBackCelcius);
+
+let FahrenheitLink = document.querySelector("#Fahrenheit-link");
+FahrenheitLink.addEventListener("click", changeToFahrenheit);
+
+searchCity("Lagos");
+
+
+
+
+// function tempButton(position){
+//     //This data of longitude and latitude is provided by the geolocation navigator and is
+//     //passed to the API as strings.
+//     let apiKey = "76a578de3ab86388efb5bf00ea5c9bf1";
+//     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+
+//     //axios helps to use the API then calls the giveTemp function above
+//     axios.get(apiUrl).then(giveTemp);
+// }
+// //navigator function is nested in this function
+// function temperateRedir(){
+//     navigator.geolocation.getCurrentPosition(tempButton);
+// }
+
+// //listens to the button html element
+// let buttonClick = document.querySelector("button");
+// buttonClick.addEventListener("click", temperateRedir);
